062d2e60c7d54976bb74225da0f7f6d6e0b59d15
karpov-dev <uladzimir.karpau@gmail.com>
2021-11-23 23:01:28 +0300

    v. 0.4.2

--- Songs.py
+++ Songs.py
@@ -23,7 +23,7 @@ class Songs:
             counter += 1
             full_info = base_info.fetch_track()
             self.tracks.append(full_info)
-            self.logger.message(str(counter) + '/' + str(len(self.liked_songs)) + ' : (' + str(round(counter / len(self.liked_songs), 3)) + '%) ' + ': ' + full_info.title)
+            self.logger.message(str(counter) + '/' + str(len(self.liked_songs)) + ' : (' + str(round(counter / len(self.liked_songs) * 100, 3) ) + '%) ' + ': ' + full_info.title)
 
         self.logger.message('FETCHING DATA WAS SUCCESS')
 
